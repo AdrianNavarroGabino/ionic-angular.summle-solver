8e0898cf435b2fbcf3aca84f4cf608fda9a048ff
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-08-28 11:40:16 +0200

    Fetch summle

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   expected: number;
   n1: number;
   n2: number;
@@ -15,5 +15,26 @@ export class HomePage {
   n6: number;
 
   constructor() {}
+
+  ngOnInit() {
+    (async () => {
+      const response = await fetch('https://cors-anywhere.herokuapp.com/https://summle.net/');
+      const text = await response.text();
+      text.split("\n").forEach(l => {
+        if(l.includes("window.puzzString = ")) {
+          const numbers = l.replace(/(\")|;|(window.puzzString = )/g, "").split(",");
+          
+          this.n1 = Number(numbers[0]);
+          this.n2 = Number(numbers[1]);
+          this.n3 = Number(numbers[2]);
+          this.n4 = Number(numbers[3]);
+          this.n5 = Number(numbers[4]);
+          this.n6 = Number(numbers[5]);
+          this.expected = Number(numbers[6]);
+        }
+      })
+    })();
+  }
+  
 
 }
