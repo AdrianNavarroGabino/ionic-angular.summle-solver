254af087323d1659190318cfbd655e0180545508
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-08-30 09:25:20 +0200

    Exit on back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+    this.platform.backButton.subscribeWithPriority(10, () => {
+        App.exitApp();
+    });
+  }
 }
